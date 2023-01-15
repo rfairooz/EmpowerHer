6d6cbaca938c178118cc568fe61c7af1c2c9ef86
mkawak <mkawak.cs@gmail.com>
2023-01-15 02:19:20 -0800

    Uploading files

--- rosehack_proj/src/pages/index.tsx
+++ rosehack_proj/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 const index = () => {
-  return <div className="text-4xl bg-red-500">Hello world</div>;
+  return <div className="text-4xl bg-red-500">HELLOOOO</div>;
 };
 
 export default index;
